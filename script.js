e6c9ace8c6a8cd66fb69de6b02056138dbe2e7eb
andyreya <83035508+andyreya@users.noreply.github.com>
2021-06-24 18:38:55 +0000

    trailing spaces and new line

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// makes action occur when button is clicked 
+// makes action occur when button is clicked
 
 document.getElementById('button').addEventListener('click', calculate)
 
-// creates variables 
+// creates variables
 
 let first = 0
 let second = 0
@@ -10,15 +10,15 @@ let answer = 0
 let counter = 0
 let negative = false
 
-// creates the function and sets what will happen when the button is clicked 
+// creates the function and sets what will happen when the button is clicked
 
 function calculate () {
   first = document.getElementById('input1').value
   second = document.getElementById('input2').value
   first = parseInt(first)
   second = parseInt(second)
-  answer = 0 
-  negative = false 
+  answer = 0
+  negative = false
 
   if (first < 0) {
     negative = !negative
@@ -36,6 +36,6 @@ function calculate () {
   if (negative) {
     answer = 0 - answer
   }
-  
+
   document.getElementById('answer').innerHTML = 'Answer = ' + answer
 }
